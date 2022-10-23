c0413a6d681b47355451602bd66f1c07dedb6049
tlam89 <lam.th@northeastern.edu>
2022-10-23 09:58:10 -0400

    Created Search By: ID/Name/Date, and ListView for summary, like button to add to RecylerView

--- app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
+++ app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class ChemAPI_Activity extends AppCompatActivity {
-    WebView rscWeb;
+
+    Button launch_WebService_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chem_api);
+
+        launch_WebService_button = findViewById(R.id.website_button);
+
+
+
     }
 }
