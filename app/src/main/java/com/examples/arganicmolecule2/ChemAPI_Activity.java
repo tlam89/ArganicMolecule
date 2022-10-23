d55e84f862941a525c6cdb7a2ae169a2332a6617
YangliLiu <liu.yangl@northeastern.edu>
2022-10-23 03:55:00 -0400

    Image Display from URL - final commit

--- app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
+++ app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
@@ -2,8 +2,11 @@ package com.examples.arganicmolecule2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class ChemAPI_Activity extends AppCompatActivity {
     WebView rscWeb;
@@ -12,5 +15,17 @@ public class ChemAPI_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chem_api);
+
+        // Wire up the Molecule of the Month button
+        Button mtn = (Button) findViewById(R.id.button3);
+        mtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(ChemAPI_Activity.this, PBD_Date_Activity.class);
+                startActivity(intent1);
+            }
+        });
+
+
     }
 }
