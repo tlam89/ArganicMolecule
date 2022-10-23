7763f225e13069208fdabd56a2f7c31283dba843
tlam89 <lam.th@northeastern.edu>
2022-10-23 10:33:50 -0400

    Created Search By: ID/Name/Date, and ListView for summary, like button to add to RecylerView

--- app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
+++ app/src/main/java/com/examples/arganicmolecule2/ChemAPI_Activity.java
@@ -2,7 +2,9 @@ package com.examples.arganicmolecule2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 
