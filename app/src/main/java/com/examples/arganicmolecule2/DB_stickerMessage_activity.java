0c20a69b259ad533827d9cf42055c7af9bc7db41
saremi-a <saremi.a@northeastern.edu>
2022-11-04 13:29:59 -0700

    finished the userlogin and waiting for the database to be created

---
+++ app/src/main/java/com/examples/arganicmolecule2/DB_stickerMessage_activity.java
@@ -0,0 +1,14 @@
+package com.examples.arganicmolecule2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DB_stickerMessage_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db_sticker_message);
+    }
+}
--- app/src/main/java/com/examples/arganicmolecule2/MainActivity.java
+++ app/src/main/java/com/examples/arganicmolecule2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     Button RSChem_Button;
+    Button FirebaseBtn;
     LinearLayout buttonsLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
         RSChem_Button = findViewById(R.id.PubChem_button);
 
         RSChem_Button.setOnClickListener(view -> openRSChemWebViewScreen());
+        //DataBase
+        FirebaseBtn= findViewById(R.id.Firebase_button);
+        FirebaseBtn.setOnClickListener(view -> openFirebaseViewScreen());
+
     }
 
     public void openRSChemWebViewScreen() {
         Intent openRSChemWebViewActivity = new Intent(this, ChemAPI_Activity.class);
         startActivity(openRSChemWebViewActivity);
+    }
+    public void openFirebaseViewScreen(){
+        Intent openFirebase_activity = new Intent(MainActivity.this,DB_authentication_activity.class);
+        startActivity(openFirebase_activity);
     }
 }
