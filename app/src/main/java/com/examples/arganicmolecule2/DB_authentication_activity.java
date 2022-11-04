1883af2b9d552e69576bd13fefe9c5c2d751a2a2
saremi-a <saremi.a@northeastern.edu>
2022-11-04 14:53:03 -0700

    adding the firebase link to the userlogin

--- app/src/main/java/com/examples/arganicmolecule2/DB_authentication_activity.java
+++ app/src/main/java/com/examples/arganicmolecule2/DB_authentication_activity.java
@@ -11,8 +11,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class DB_authentication_activity extends AppCompatActivity {
-    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("");
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://arganicmolecule2-66023-default-rtdb.firebaseio.com");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,44 +33,44 @@ public class DB_authentication_activity extends AppCompatActivity {
         final TextView registerNow= findViewById(R.id.registerNowBtn);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                                         @Override
+                                         public void onClick(View view) {
 
-                final String phoneTxt= phone.getText().toString();
-                final String passwordTxt= password.getText().toString();
-                if(phoneTxt.isEmpty() || passwordTxt.isEmpty()){
-                    Toast.makeText(DB_authentication_activity.this,"Please enter your mobile" +
-                            " or password",Toast.LENGTH_SHORT).show();
-                }else{
-                    //adding to the firebase
-                    databaseRefrence.child("users").addListenerForSingleValueEventListener(){
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot){
-                            if(snapshot.hasChild(phoneTxt)){
-                                final String getPassword= snapshot.chil(phoneTxt).child("password").getValue(String.class);
-                                if(getPassword.equals(passwordTxt)){
-                                    Toast.makeText(DB_authentication_activity.this,"Successfully logged in",
-                                            Toast.LENGTH_SHORT).show();
-                                    startActivity( new Intent(DB_authentication_activity.this,DB_stickerMessage_activity.class));
-                                    finish();
-                                }else{
-                                    Toast.makeText(DB_authentication_activity.this,"Wrong password",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                            }else{
-                            Toast.makeText(DB_authentication_activity.this,"Wrong mobile number",
-                                    Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error){
+                                             final String phoneTxt = phone.getText().toString();
+                                             final String passwordTxt = password.getText().toString();
+                                             if (phoneTxt.isEmpty() || passwordTxt.isEmpty()) {
+                                                 Toast.makeText(DB_authentication_activity.this, "Please enter your mobile" +
+                                                         " or password", Toast.LENGTH_SHORT).show();
+                                             } else {
+                                                 //adding to the firebase
+                                                 databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                                                     @Override
+                                                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                         if (snapshot.hasChild(phoneTxt)) {
+                                                             final String getPassword = snapshot.child(phoneTxt).child("password").getValue(String.class);
+                                                             if (getPassword.equals(passwordTxt)) {
+                                                                 Toast.makeText(DB_authentication_activity.this, "Successfully logged in",
+                                                                         Toast.LENGTH_SHORT).show();
+                                                                 startActivity(new Intent(DB_authentication_activity.this, DB_stickerMessage_activity.class));
+                                                                 finish();
+                                                             } else {
+                                                                 Toast.makeText(DB_authentication_activity.this, "Wrong password",
+                                                                         Toast.LENGTH_SHORT).show();
+                                                             }
+                                                         } else {
+                                                             Toast.makeText(DB_authentication_activity.this, "Wrong Mobile number",
+                                                                     Toast.LENGTH_SHORT).show();
+                                                         }
+                                                     }
 
-                        }
-                }
-            }
+                                                     @Override
+                                                     public void onCancelled(@NonNull DatabaseError error) {
+
+                                                     }
+                                                 });
+                                           }
+                                      }
         });
-
         registerNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,7 +78,5 @@ public class DB_authentication_activity extends AppCompatActivity {
 
             }
         });
-
-
     }
 }
