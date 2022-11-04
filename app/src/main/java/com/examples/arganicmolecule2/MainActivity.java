bd7db3f0267bf18744b240e6455a174bd418cb6a
YangliLiu <liu.yangl@northeastern.edu>
2022-11-04 17:20:05 -0400

    Adding Activity3 history empty class and firebase database

--- app/src/main/java/com/examples/arganicmolecule2/MainActivity.java
+++ app/src/main/java/com/examples/arganicmolecule2/MainActivity.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     Button RSChem_Button;
     LinearLayout buttonsLayout;
+    Button Firebase_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RSChem_Button = findViewById(R.id.PubChem_button);
-
         RSChem_Button.setOnClickListener(view -> openRSChemWebViewScreen());
+
+        Firebase_button = findViewById(R.id.Firebase_button);
     }
 
     public void openRSChemWebViewScreen() {
---
+++ app/src/main/java/com/examples/arganicmolecule2/history.java
@@ -0,0 +1,24 @@
+package com.examples.arganicmolecule2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
