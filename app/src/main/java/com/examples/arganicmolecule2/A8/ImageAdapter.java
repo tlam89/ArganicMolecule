cb338a7af93a406a1ed06b1a4dc8653dacf5de34
YangliLiu <liu.yangl@northeastern.edu>
2022-11-05 17:34:28 -0400

    Change Layout and Display Chosen Image

--- app/src/main/java/com/examples/arganicmolecule2/A8/ImageAdapter.java
+++ app/src/main/java/com/examples/arganicmolecule2/A8/ImageAdapter.java
@@ -1,13 +1,18 @@
 package com.examples.arganicmolecule2.A8;
 
+
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -19,10 +24,16 @@ import java.util.List;
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private List<sticker> stickerList;
     private Context context;
+    private LinkClickListener listener;
 
-    public ImageAdapter (List<sticker> stickerList, Context context){
+
+    public ImageAdapter (List<sticker> stickerList, Context context, LinkClickListener listener){
         this.stickerList = stickerList;
         this.context = context;
+        this.listener = listener;
+    }
+    public void setOnItemClickListener(LinkClickListener listener) {
+        this.listener = listener;
     }
 
     @NonNull
@@ -30,7 +41,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.sticker_layout, parent, false);
         //View view = LayoutInflater.from(context).inflate(R.layout.sticker_layout, parent, false);
-        return new ImageViewHolder(view);
+        return new ImageViewHolder(view, listener);
     }
 
     @Override
@@ -50,10 +61,20 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
         private TextView name;
         private ImageView image;
 
-        public ImageViewHolder(@NonNull View itemView) {
+        public ImageViewHolder(@NonNull View itemView, LinkClickListener listener) {
             super(itemView);
             name = itemView.findViewById(R.id.Text);
             image = itemView.findViewById(R.id.StickerImage);
+
+            itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    int position = getLayoutPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
+                    }
+                }
+            });
+
         }
     }
 }
