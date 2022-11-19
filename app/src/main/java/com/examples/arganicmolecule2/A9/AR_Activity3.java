e4dd2a49551032332c71aa5e21865e19133dfce0
tlam89 <lam.th@northeastern.edu>
2022-11-19 08:57:41 -0500

    "fixed dependencies in gradle"

--- app/src/main/java/com/examples/arganicmolecule2/A9/AR_Activity3.java
+++ app/src/main/java/com/examples/arganicmolecule2/A9/AR_Activity3.java
@@ -2,8 +2,13 @@ package com.examples.arganicmolecule2.A9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 import com.examples.arganicmolecule2.R;
@@ -16,49 +21,100 @@ import com.google.ar.sceneform.ux.TransformableNode;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+
 public class AR_Activity3 extends AppCompatActivity {
     private ArFragment arFragment;
     StorageReference storageReference;
-    String model;
+    String model_path;
+
+    Uri glbUri;
+    File glb_path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar3);
-
-        storageReference= FirebaseStorage.getInstance().getReference("Amino Acids/"+"A.glb");
-        model = storageReference.toString();
-        Toast.makeText(AR_Activity3.this, model , Toast.LENGTH_LONG).show();
-
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-        arFragment.onCreate(savedInstanceState);
+        storageReference= FirebaseStorage.getInstance().getReference("Amino Acids/A.glb");
+        model_path = "https://firebasestorage.googleapis.com/v0/b/arganicmolecule2-66023.appspot.com/o/Amino%20Acids%2FA.glb?alt=media&token=7f57e996-74b1-4521-a90a-f4a0b09f4b4f";
+        Log.i("model_path", model_path);
+        //Create Uri for ArFragment
+        Long glbID = doInBackground();
+        Log.i("GLB_ID", glbID.toString());
+        onPostExecute(glbID);
+        Log.i("GLB_ID2", glbID.toString());
+        receiveDownloadId(glbID);
+        Log.i("GLB_ID3", glb_path.toString());
 
-        // adding listener for detecting plane.
+
+
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
 
-            // adding model to the scene
             ModelRenderable.builder()
-                    .setSource(this, RenderableSource.builder().setSource(
-                                    this, Uri.parse(model), RenderableSource.SourceType .GLB)
+                    .setSource(this, RenderableSource.builder().setSource(this, Uri.parse("G.glb"), RenderableSource.SourceType.GLB)
                             .setScale(0.1f)
                             .setRecenterMode(RenderableSource.RecenterMode.ROOT)
                             .build())
                     .build()
-                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable));
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(
+                            throwable -> {
+                                Toast toast =
+                                        Toast.makeText(this, "Unable to load GLB renderable", Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                                return null;
+                            });
         });
     }
 
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
         AnchorNode node = new AnchorNode(anchor);
+        arFragment.getArSceneView().getScene().addChild(node);
+
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
-        transformableNode.setParent(node); // need to attach to parent
+        transformableNode.setParent(node);
         transformableNode.setRenderable(modelRenderable);
         transformableNode.getLight();
-
-        arFragment.getArSceneView().getScene().addChild(node); // adddding only parent node, so the child nodes will be added automatically
         transformableNode.select();
     }
+
+    private Long doInBackground() {
+        DownloadManager downloadManager = (DownloadManager) AR_Activity3.this.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(model_path);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle("A.glb");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "A.glb");
+        return downloadManager.enqueue(request);
+    }
+
+    private void onPostExecute(Long result) {
+        AR_Activity3.this.receiveDownloadId(result);
+    }
+
+    private void receiveDownloadId(Long downloadId) {
+//        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+//        glbUri = downloadManager.getUriForDownloadedFile(downloadId);
+//        glb_path = new File(glbUri.getPath());
+//        return glb_path;
+        String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        Toast.makeText(AR_Activity3.this, filepath, Toast.LENGTH_LONG).show();
+
+        File directory = new File(filepath.toString());
+        File[] files = directory.listFiles();
+        for (File f: files) {
+            if (f.toString().equals(filepath +"/A.glb")) {
+                Toast.makeText(AR_Activity3.this, f.toString(), Toast.LENGTH_LONG).show();
+                glb_path = f;
+                break;
+            }
+        }
+    }
+
+
 
     @Override
     protected void onResume() {
@@ -70,5 +126,20 @@ public class AR_Activity3 extends AppCompatActivity {
         super.onPause();
     }
 }
+
+
+
+//        Method 1: failed
+//        String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+//        Toast.makeText(AR_Activity3.this, filepath, Toast.LENGTH_LONG).show();
+
+//        File directory = new File(filepath.toString());
+//        File[] files = directory.listFiles();
+//        for (File f: files) {
+//            if (f.toString().equals(filepath +"/100D.glb")) {
+//                Toast.makeText(AR_Activity3.this, f.toString(), Toast.LENGTH_LONG).show();
+//                model_path = f.toString();
+//            }
+//        }
 
 
