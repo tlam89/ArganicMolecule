bcbadbf8f6816a8177ae4ac11c9f80ec269648cb
Pranit Brahmbhatt <pranit@esamudaay.com>
2022-11-20 00:27:16 -0500

    updated note adapter

--- app/src/main/java/com/examples/arganicmolecule2/A7/NoteAdapter.java
+++ app/src/main/java/com/examples/arganicmolecule2/A7/NoteAdapter.java
@@ -1,6 +1,5 @@
 package com.examples.arganicmolecule2.A7;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +12,7 @@ import com.examples.arganicmolecule2.R;
 
 import java.util.ArrayList;
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NotesViewHolder> {
 
     private final ArrayList<Note> notes;
     //private Context context;
@@ -25,14 +24,14 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
 
     @NonNull
     @Override
-    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NoteAdapter.NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_data, parent,
                 false);
-        return new NoteViewHolder(view);
+        return new NoteAdapter.NotesViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NoteAdapter.NotesViewHolder holder, int position) {
         holder.formula.setText(notes.get(position).getFormula());
         holder.formula_weight.setText(notes.get(position).getFormula_weight());
         holder.id.setText(String.valueOf(notes.get(position).getId()));
@@ -44,7 +43,11 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
         return notes.size();
     }
 
-    public class NotesViewHolder extends RecyclerView.ViewHolder {
+    public static class NotesViewHolder extends RecyclerView.ViewHolder {
+        private TextView formula;
+        private TextView formula_weight;
+        private TextView id;
+        private TextView name;
         public NotesViewHolder(@NonNull View notesView) {
             super(notesView);
             TextView formula = notesView.findViewById(R.id.textView_formula);
