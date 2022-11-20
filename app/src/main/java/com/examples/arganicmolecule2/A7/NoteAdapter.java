3a2549c727c74f0d5870dfd3c447a415a454b208
Pranit Brahmbhatt <pranit@esamudaay.com>
2022-11-20 00:19:22 -0500

    data pushed and fetchec from firebase for webservice activity

--- app/src/main/java/com/examples/arganicmolecule2/A7/NoteAdapter.java
+++ app/src/main/java/com/examples/arganicmolecule2/A7/NoteAdapter.java
@@ -1,8 +1,10 @@
 package com.examples.arganicmolecule2.A7;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,9 +13,9 @@ import com.examples.arganicmolecule2.R;
 
 import java.util.ArrayList;
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder>{
+public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
 
-    private ArrayList<Note> notes;
+    private final ArrayList<Note> notes;
     //private Context context;
 
     public NoteAdapter(ArrayList<Note> notes) {
@@ -24,7 +26,8 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder>{
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_data, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_data, parent,
+                false);
         return new NoteViewHolder(view);
     }
 
@@ -32,12 +35,22 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder>{
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
         holder.formula.setText(notes.get(position).getFormula());
         holder.formula_weight.setText(notes.get(position).getFormula_weight());
-        holder.id .setText(String.valueOf(notes.get(position).getId()));
+        holder.id.setText(String.valueOf(notes.get(position).getId()));
         holder.name.setText(notes.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
         return notes.size();
+    }
+
+    public class NotesViewHolder extends RecyclerView.ViewHolder {
+        public NotesViewHolder(@NonNull View notesView) {
+            super(notesView);
+            TextView formula = notesView.findViewById(R.id.textView_formula);
+            TextView formularWeight = notesView.findViewById(R.id.textView_formular_weight);
+            TextView id = notesView.findViewById(R.id.textView_id);
+            TextView name = notesView.findViewById(R.id.textView_name);
+        }
     }
 }
